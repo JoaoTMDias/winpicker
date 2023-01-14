c1a310323add1e81169ef3e019e50e20a7a72572
João Dias <contacto@joaodias.me>
2023-01-14 12:08:48 +0000

    Migrated to Tauri (#1)

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,10 +1,1 @@
 export { }
-
-declare global {
-  interface Window {
-    // Expose API through preload script
-    fs: typeof import('fs')
-    ipcRenderer: import('electron').IpcRenderer
-    removeLoading: () => void
-  }
-}
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
-import { render } from 'react-dom'
-import App from './app'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./app";
+import "./index.css";
 
-render(<App />, document.getElementById('root')!)
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(<App />);
